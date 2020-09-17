c16c9ab2df176e99b66cf9a0ad9adaa4b21e9993
margaret mahan <maggiemahan@flywheel.io>
2020-09-17 12:38:22 -0500

    FIX: change demo scripts to reflect nifti datatype.

--- demo/populate_instance.py
+++ demo/populate_instance.py
@@ -9,19 +9,22 @@ def run(API_KEY):
     """Orchestrate instance data population."""
 
     # Setup
-    GROUP_ID = 'tbi-extractor'
-    GROUP_LABEL = 'TBI Extractor Demo'
-    PROJECT_LABEL = 'tbi-extractor'
+    GROUP_ID = "tbi-extractor"
+    GROUP_LABEL = "TBI Extractor Demo"
+    PROJECT_LABEL = "tbi-extractor"
     fw = flywheel.Client(api_key=API_KEY)
 
-    # Create (or capture) group and project
-    if not fw.groups.find_first(f"label={GROUP_LABEL}"):
-       GROUP_ID = fw.add_group(flywheel.Group(GROUP_ID, GROUP_LABEL))
+    # Create group and project
+    if fw.groups.find_first(f"label={GROUP_LABEL}"):
+        GROUP_ID = fw.add_group(flywheel.Group(GROUP_ID, GROUP_LABEL))
 
     group = fw.get(GROUP_ID)
 
-    if group.projects.find_first(f'label={PROJECT_LABEL}'):
-        project = group.projects.find_first(f'label={PROJECT_LABEL}')
+    if group.projects.find_first(f"label={PROJECT_LABEL}"):
+        print(
+            "tbi-extractor project exists. No attempts to establish coherence will be made."
+        )
+        project = group.projects.find_first(f"label={PROJECT_LABEL}")
     else:
         project = group.add_project(label=f"{PROJECT_LABEL}")
 
@@ -30,32 +33,39 @@ def run(API_KEY):
     for demo_subject in demo_subjects:
 
         # Create subject from list of demo data subjects
-        SUBJECT_LABEL = demo_subject.split('/')[-1]
-        if not project.subjects.find_first(f'label={SUBJECT_LABEL}'):
+        SUBJECT_LABEL = demo_subject.split("/")[-1]
+        if not project.subjects.find_first(f"label={SUBJECT_LABEL}"):
             subject = project.add_subject(label=SUBJECT_LABEL)
         else:
-            subject = project.subjects.find_first(f'label={SUBJECT_LABEL}')
+            subject = project.subjects.find_first(f"label={SUBJECT_LABEL}")
 
         # Create session and acquisition for the subject
         SESSION_LABEL = "ses-01"
-        if not subject.sessions.find_first(f'label={SESSION_LABEL}'):
+        if not subject.sessions.find_first(f"label={SESSION_LABEL}"):
             session = subject.add_session(label=SESSION_LABEL)
         else:
             session = subject.sessions.find_first(f"label={SESSION_LABEL}")
 
         ACQUISITION_LABEL = "noncontrast-head-ct"
-        if not session.acquisitions.find_first(f'label={ACQUISITION_LABEL}'):
+        if not session.acquisitions.find_first(f"label={ACQUISITION_LABEL}"):
             acquisition = session.add_acquisition(label=ACQUISITION_LABEL)
         else:
-            acquisition = session.acquisitions.find_first(f'label={ACQUISITION_LABEL}')
+            acquisition = session.acquisitions.find_first(f"label={ACQUISITION_LABEL}")
 
         # Add files to acquisition for the subject
         files = glob.glob(demo_subject + "/*")
         for file in files:
             if not acquisition.get_file(file):
 
-                if file.endswith('.zip'):
-                    metadata = json.dumps({'type': 'dicom'})
-                    fw.upload_file_to_acquisition(acquisition.id, file, metadata=metadata)
+                if file.endswith(".zip"):
+                    metadata = json.dumps({"type": "dicom"})
+                    fw.upload_file_to_acquisition(
+                        acquisition.id, file, metadata=metadata
+                    )
+                elif file.endswith(".nii.gz"):
+                    metadata = json.dumps({"type": "nifti"})
+                    fw.upload_file_to_acquisition(
+                        acquisition.id, file, metadata=metadata
+                    )
                 else:
                     fw.upload_file_to_acquisition(acquisition.id, file)
--- demo/run_gear.py
+++ demo/run_gear.py
@@ -6,14 +6,14 @@ def run(API_KEY):
     """Orchestrate gear run."""
 
     # Setup
-    PROJECT_LABEL = 'tbi-extractor'
+    PROJECT_LABEL = "tbi-extractor"
     fw = flywheel.Client(api_key=API_KEY)
 
     # Load the project
-    project = fw.projects.find_first(f'label={PROJECT_LABEL}')
+    project = fw.projects.find_first(f"label={PROJECT_LABEL}")
 
     # Load the Gear
-    tbi_extractor_gear = fw.lookup('gears/tbi-extractor')
+    tbi_extractor_gear = fw.lookup("gears/tbi-extractor")
 
     # For each session in the project, find the inputs and run tbi-extractor
     for session in project.sessions.iter():
@@ -25,18 +25,21 @@ def run(API_KEY):
             if len(acq.files) == 2:
 
                 for file in acq.files:
-                    if file.type == 'text':
+                    if file.type == "text":
                         radiology_report = file
-                    if file.type == 'dicom':
+                    if (file.type == "dicom") or (file.type == "nifti"):
                         corresponding_scan = file
 
-                inputs = {"radiology_report": radiology_report,
-                          "corresponding_scan": corresponding_scan}
+                inputs = {
+                    "radiology_report": radiology_report,
+                    "corresponding_scan": corresponding_scan,
+                }
 
-                config = {"save_target_phrases": True,
-                          "save_modifier_phrases": True}
+                config = {"save_target_phrases": True, "save_modifier_phrases": True}
 
-                tbi_extractor_gear.run(analysis_label='Demo',
-                                       config=config,
-                                       inputs=inputs,
-                                       destination=session)
+                tbi_extractor_gear.run(
+                    analysis_label="Demo",
+                    config=config,
+                    inputs=inputs,
+                    destination=session,
+                )
